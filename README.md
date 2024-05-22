3d178a27fe78cc62f6c79595c38b1aaa8acbe7ec
JQK1806 <king.jos@northeastern.edu>
2024-05-22 14:35:00 -0400

    Initial prompts corrected, README added
    Initial prompts now introduce the model as ReX, prior was introducing the user as ReX.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Gemini_Explorer
+Gemini Explorer is a Streamlit based chat interface that is powered by Google Gemini. Users can interact
+with the model through text input. This application uses Vertex AI for the model initialization and communication.
+
+# Features
+Chat Interface: Users interact with the model through typed messages in the chat box. Chat history is maintained,
+displaying both the user's and model's messages.
+Personalized Greetings: Users can input their name to have a personalized greeting.
+
+# Setup
+1. Clone the repository
+2. Install dependencies in requirements.txt
+3. Run the Streamlit app : streamlit run gemini_explorer.py
+
+# Usage
+1. Run the Streamlit app
+2. Enter name (optionl)
+3. Press "Ready to chat"
--- gemini_explorer.py
+++ gemini_explorer.py
@@ -3,20 +3,25 @@ import streamlit as st
 from vertexai.preview import generative_models
 from vertexai.preview.generative_models import GenerativeModel, Part, Content, ChatSession
 
+# Initialize VertexAI project
 project = "geminiexplorer-424016"
 vertexai.init(project=project)
 
+# Initialize generative model config
 config = generative_models.GenerationConfig(
     temperature=0.4
 )
 
+# Initialize the generative model
 model = GenerativeModel(
     "gemini-pro",
     generation_config=config
 )
 
+# Start a chat session
 chat = model.start_chat()
 
+# Send a message in the chat and display in the Streamlit app
 def llm_function(chat: ChatSession, query):
     response = chat.send_message(query)
     output = response.candidates[0].content.parts[0].text
@@ -64,10 +69,10 @@ if len(st.session_state.messages) == 0:
     # Button to trigger the initial prompt
     if st.button("Ready to Chat"):
         if user_name:
-            personalized_greeting = f"👋 Hello, I'm {user_name}! An assistant powered by Google Gemini."
+            personalized_greeting = f"Introduce yourself as ReX, an assistant powered by Google Gemini. You are talking with {user_name}. Use emojis to be interactive."
             llm_function(chat, personalized_greeting)
         else:
-            default_greeting = f"👋 Hello! I'm ReX, an assistant powered by Google Gemini."
+            default_greeting = "Introduce yourself as ReX, an assistant powered by Google Gemini. You talk like a pirate."
             llm_function(chat, default_greeting)
 
 # For capture user input
